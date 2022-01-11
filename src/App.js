a8d91c2fe171ed503196c96ec33288e2198ce0d1
Joe6276 <joendambuki16@gmail.com>
2022-01-11 16:24:30 +0300

    simple paginatiojn using an API data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import{useState, useEffect} from 'react'
+import axios from 'axios'
+import Post from './Components/Post';
+import Pagination from './Components/Pagination'
+
 
 function App() {
+
+const [posts,setPosts] = useState([]);
+const [loading,setLoading]= useState(false)
+const [ current, setCurrent]= useState(1);
+const [postperpage]= useState(10) 
+const paginate=(pageNumber)=>{ setCurrent(pageNumber)}
+
+useEffect(() => {
+  const fetchPost =async ()=>{
+      setLoading(true)
+      const res=  await axios.get('https://jsonplaceholder.typicode.com/posts')
+      setPosts(res.data)
+      setLoading(false)
+  }
+  fetchPost();
+
+}, [])
+
+
+
+console.log(posts)
+
+const indexoflastpost= current * postperpage;
+const indexofFirstpost= indexoflastpost-postperpage;
+const currentposts=posts.slice(indexofFirstpost, indexoflastpost)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+  <h1 className='text-primary mb-3'> Learning Pagination</h1>
+
+  <Post  posts={currentposts} loading={loading}/>
+  <Pagination postPerPage= {postperpage} totalPosts={posts.length} paginate={paginate} />
     </div>
   );
 }
